9821a4c5796b90e2b8ad73cb9132f2f52e859af5
Luis Rishi Puno <LRPuno@gmail.com>
2018-10-27 09:49:03 -0400

    Updates

---
+++ precache-manifest.255775547db0b5ba5728cdd6365e3314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "c91c567fe1bde83e092b",
+    "url": "/clicky-game/static/js/main.c91c567f.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/clicky-game/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "c91c567fe1bde83e092b",
+    "url": "/clicky-game/static/css/main.ce9fdcf0.chunk.css"
+  },
+  {
+    "revision": "b05a5eab82500fa3116c0bf656fc1d5a",
+    "url": "/clicky-game/index.html"
+  }
+];
